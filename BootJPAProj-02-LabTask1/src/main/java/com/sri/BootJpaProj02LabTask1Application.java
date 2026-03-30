fb63f144adc1068a189234a5a1ae9f15af7968af
Sritam369 <chakrabarttysritam@gmail.com>
2026-03-30 15:39:01 +0530

    spring boot data jpa labtask

---
+++ BootJPAProj-02-LabTask1/src/main/java/com/sri/BootJpaProj02LabTask1Application.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj02LabTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj02LabTask1Application.class, args);
+	}
+
+}
