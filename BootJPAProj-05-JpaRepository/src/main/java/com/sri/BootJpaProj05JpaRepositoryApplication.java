a392a7fc69b620a9e3205838736c12df176e8eb5
Sritam369 <chakrabarttysritam@gmail.com>
2026-04-06 22:06:37 +0530

    metadata

---
+++ BootJPAProj-05-JpaRepository/src/main/java/com/sri/BootJpaProj05JpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj05JpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj05JpaRepositoryApplication.class, args);
+	}
+
+}
