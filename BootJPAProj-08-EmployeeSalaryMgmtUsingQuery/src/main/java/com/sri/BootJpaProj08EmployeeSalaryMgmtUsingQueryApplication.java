9f8138061881b70aba703374cb2379c3b5af5814
Sritam369 <chakrabarttysritam@gmail.com>
2026-04-08 15:57:46 +0530

    metadata

---
+++ BootJPAProj-08-EmployeeSalaryMgmtUsingQuery/src/main/java/com/sri/BootJpaProj08EmployeeSalaryMgmtUsingQueryApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj08EmployeeSalaryMgmtUsingQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj08EmployeeSalaryMgmtUsingQueryApplication.class, args);
+	}
+
+}
