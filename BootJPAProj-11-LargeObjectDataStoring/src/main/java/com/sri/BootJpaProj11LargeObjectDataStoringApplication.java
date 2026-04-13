1ece59c577c005c09151ecb92145a281591c024d
Sritam369 <chakrabarttysritam@gmail.com>
2026-04-13 21:33:59 +0530

    lob

---
+++ BootJPAProj-11-LargeObjectDataStoring/src/main/java/com/sri/BootJpaProj11LargeObjectDataStoringApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj11LargeObjectDataStoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj11LargeObjectDataStoringApplication.class, args);
+	}
+
+}
