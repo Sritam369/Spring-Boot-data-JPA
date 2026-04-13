7bb98aac571b9052012ba0b9356846ff4b67df23
Sritam369 <chakrabarttysritam@gmail.com>
2026-04-13 22:06:40 +0530

    metadata

---
+++ BootJPAProj-12-DateTimeApi/src/main/java/com/sri/BootJpaProj12DateTimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj12DateTimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj12DateTimeApiApplication.class, args);
+	}
+
+}
