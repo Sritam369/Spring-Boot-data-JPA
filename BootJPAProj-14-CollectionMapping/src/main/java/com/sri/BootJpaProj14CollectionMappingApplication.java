0455358bec319839e42ccda0dae71118178ed191
Sritam369 <chakrabarttysritam@gmail.com>
2026-04-15 18:28:44 +0530

    metadata

---
+++ BootJPAProj-14-CollectionMapping/src/main/java/com/sri/BootJpaProj14CollectionMappingApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj14CollectionMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj14CollectionMappingApplication.class, args);
+	}
+
+}
---
+++ BootJPAProj-14-CollectionMapping/src/test/java/com/sri/BootJpaProj14CollectionMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaProj14CollectionMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
