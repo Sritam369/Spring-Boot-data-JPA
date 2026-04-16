6ea54b42738d4789954419001d4d949e88cb6d47
Sritam369 <chakrabarttysritam@gmail.com>
2026-04-16 17:34:58 +0530

    metadata

---
+++ BootJPAProj-16-OneToManyMapping/src/main/java/com/sri/BootJpaProj16OneToManyMappingApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj16OneToManyMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj16OneToManyMappingApplication.class, args);
+	}
+
+}
