fed9b05aae72f57cab959091bcf1a6245bab5fa6
Sritam369 <chakrabarttysritam@gmail.com>
2026-04-27 12:42:10 +0530

    metadata

---
+++ BootJPAProj-21-ManyToManyMappingLabTask/src/main/java/com/sri/BootJpaProj21ManyToManyMappingLabTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaProj21ManyToManyMappingLabTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaProj21ManyToManyMappingLabTaskApplication.class, args);
+	}
+
+}
